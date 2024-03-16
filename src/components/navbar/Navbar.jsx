1de643752c6dcccec97ce024cba27616cb4d6aa1
Robert Cheadle <robert.cheadle01@gmail.com>
2024-03-16 19:29:02 -0400

    setup basic components and layouts, error and loading, etc

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import Links from './links/links';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>
+                <Links />
+            </div>
+            <div>Tel: (000) 000-0000</div>
+            <div>Email Link</div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/components/navbar/links/links.jsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+const Links = () => {
+
+    const links = [
+        {
+            title: 'Homepage',
+            path: '/',
+        },
+        {
+            title: 'About',
+            path: '/about',
+        },
+        {
+            title: 'Contact',
+            path: '/contact',
+        },
+        {
+            title: 'cart',
+            path: '/cart',
+        },
+        {
+            title: 'FAQ',
+            path: '/faq',
+        },
+    ];
+
+    return (
+        <div>
+            {links.map((link=>(
+                <Link href={link.path} key={link.title}>{link.title}</Link>
+            )))}
+        </div>
+    )
+}
+
+export default Links;
